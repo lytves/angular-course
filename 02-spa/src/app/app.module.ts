62304a22afe456a0aabd99e6d2057606f2eed981
lytves <22356770+lytves@users.noreply.github.com>
2019-12-22 16:56:19 +0100

    heroes card init, css animated, router

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
+import {AppRoutingModule} from './app.routes'
+
+// Services
+
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,7 +22,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
