cfd2598ccdee5c3907408334ec21d21dd87c4561
lytves <22356770+lytves@users.noreply.github.com>
2019-12-29 17:04:31 +0100

    HttpClient introduction

--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe((resp: any) => {
+        this.paises = resp;
+      });
+  }
 
   ngOnInit() {
   }
